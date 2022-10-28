c5175c9cd91cd78675c5b8b93489010d8adcafaf
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-10-28 12:44:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #bin #!bash
 
 ###### Adding Files ######
-wget -O /content/Leech-Pro/app/tobrot/aria2/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
-wget -O /content/Leech-Pro/app/tobrot/aria2/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
+wget -O tobrot/aria2/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
+wget -O tobrot/aria2/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 TRACKER=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt -: https://ngosang.github.io/trackerslist/trackers_all_http.txt -: https://newtrackon.com/api/all -: https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt -: https://torrends.to/torrent-tracker-list/?download=latest | awk '$1' | tr '\n' ',' | cat`
 ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 ###### Done Addding Files ######
@@ -10,12 +10,12 @@ ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
 if [[ -n $RCLONE_CONFIG_URL ]]; then
   echo "Rclone config detected 📁📁"
-  wget -q $RCLONE_CONFIG_URL -O /content/Leech-Pro/app/rclone.conf
+  wget -q $RCLONE_CONFIG_URL -O clone.conf
 fi
 
 if [[ -n $CONFIG_ENV_URL ]]; then
   echo " Found config.env File 📁📁 "
-	wget -q $CONFIG_ENV_URL -O /content/Leech-Pro/app/config.env
+	wget -q $CONFIG_ENV_URL -O config.env
 fi
 
 
